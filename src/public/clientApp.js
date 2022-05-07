69ef08d8c888cda363d1d3790e53254fd49e7316
Greg Ward <codenlighten1@gmail.com>
2022-05-07 17:46:51 -0400

    two

--- src/public/clientApp.js
+++ src/public/clientApp.js
@@ -1,3 +1,80 @@
-const handleClick = () => {
-	fetch(`${document.getElementById("txid").value}`);
+class Meta {
+	constructor(
+		application,
+		appID,
+		workTitle,
+		authorArray,
+		authorPaymail,
+		publishers,
+		publishersID,
+		publishersPaymail
+	) {
+		(this.application = application),
+			(this.appID = appID),
+			(this.workTitle = workTitle),
+			(this.authorArray = authorArray);
+		(this.authorPaymail = authorPaymail),
+			(this.publishers = publishers),
+			(this.publishersID = publishersID),
+			(this.publishersPaymail = publishersPaymail);
+		this.publishDate = Date();
+	}
+}
+
+// meta = Object.entries(meta);
+
+const handleClick = async () => {
+	let application = "raregeneration.com";
+	let appID = "1CX54YFZcLSGzoUJHjAXNkTFh3WJRhSyH1";
+	let workTitle = document.getElementById("workTitle").value;
+	let authorArray = document.getElementById("authorArray").value;
+	let authorPaymail = document.getElementById("authorPaymail").value;
+	let publishers = [
+		document.getElementById("publishers").value,
+		"Rare Generation Publishing",
+	];
+
+	let publishersID = [
+		document.getElementById("publishersID").value,
+		"17Tibq24ahAK71a5Ffqaf5RfQpWGb5Yovb",
+	];
+
+	let publishersPaymail = [
+		document.getElementById("publishersPaymail").value,
+		"17Tibq24ahAK71a5Ffqaf5RfQpWGb5Yovb",
+	];
+	let file = document.getElementById("myFile");
+	file = await file.files[0];
+	let reader = new FileReader();
+	// reader.readAsDataURL(file);
+	reader.readAsArrayBuffer(file);
+	reader.onload = function () {
+		let result = reader.result;
+		console.log(result);
+	};
+	reader.onerror = function () {
+		console.log(reader.error);
+	};
+	let meta = new Meta(
+		application,
+		appID,
+		workTitle,
+		authorArray,
+		authorPaymail,
+		publishers,
+		publishersID,
+		publishersPaymail
+	);
+	meta = JSON.stringify(meta);
+
+	// let response = fetch(`${meta}`);
+	// let res = await response;
+	// let r = await res.json();
+	// let p = JSON.parse(r);
+	// console.log("p", p);
+	// console.log({ r });
+	// document.getElementById(
+	// 	"results"
+	// ).innerHTML = `<h4>${r.workTitle}</h4><br/><h4>${r.authorArray}</h4><br/><h4>${r.authorPaymail}</h4><br/><h4>${r.publishers}</h4><br/><h4>${r.publishersPaymail}</h4><br/>`;
 };
+
--- src/server.js
+++ src/server.js
@@ -6,22 +6,37 @@ const cors = require("cors");
 const path = require("path");
 app.use(cors());
 app.use(express.static("public"));
-app.get("*", (req, res) => {
-	let myValues = Object.values(req.params)[0];
+app.get("*", async (req, res) => {
+	// let myValues2 = req.params;
+	// console.log(myValues2);
+	let myValues = Object.entries(req.params)[0][1];
 	myValues = myValues.replace("/", "");
-	console.log(myValues);
-	getTxInfo(myValues);
+	let p = JSON.parse(myValues);
 
-	res.sendFile(path.resolve(__dirname, "public", "index.html"));
+	res.send(p);
+	let m = await main("", p);
+	console.log(m);
+	// getTxInfo(myValues);
+
+	// res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+app.post("/", async (req, res) => {
+	let myValues = await req.body;
+	console.log(myValues);
+	res.send(myValues);
+});
+// const writeTX = async (my) => {
+// 	await );
+// };
 const getTxInfo = async (x) => {
 	let myTx = await myReader(x);
 
 	return myTx;
 };
-app.get("/app", (req, res) => {
-	console.log(req.params);
-});
+// app.get("/app", (req, res) => {
+// 	console.log(req.params);
+// });
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
